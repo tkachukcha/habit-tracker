b6409c62a21fe56bdb3da0fd9d4feca8c1f40476
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-02-20 19:39:32 +0300

    Add Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,32 @@ import Profile from './app/pages/profile';
 import './App.scss';
 import Sidebar from './app/components/ui/sidebar';
 import { IconContext } from 'react-icons';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
-      <IconContext.Provider value={{ size: '3rem', color: 'white' }}>
-        <TopBar className="d-lg-none" />
-        <Container fluid className="bg-dark min-vh-100 text-light">
-          <Row>
-            <Col
-              lg={3}
-              xxl={2}
-              className="d-none d-lg-block vh-100 border-end border-altdark shadow-sm py-2 px-3"
-            >
-              <Sidebar />
-            </Col>
-            <Col lg={9} xxl={10} className="py-2 px-3">
-              <Switch>
-                <Route path="/login/:type?" component={Login} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/habits/:habitID?/:edit?" component={Habits} />
-                <Route path="/" exact component={Today} />
-              </Switch>
-            </Col>
-          </Row>
-        </Container>
-      </IconContext.Provider>
+      <TopBar className="d-lg-none" />
+      <Container fluid className="bg-dark min-vh-100 text-light">
+        <Row>
+          <Col
+            lg={3}
+            xxl={2}
+            className="d-none d-lg-block vh-100 border-end border-altdark shadow-sm py-2 px-3"
+          >
+            <Sidebar />
+          </Col>
+          <Col lg={9} xxl={10} className="py-2 px-3">
+            <Switch>
+              <Route path="/login/:type?" component={Login} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/habits/:habitID?/:edit?" component={Habits} />
+              <Route path="/" exact component={Today} />
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+      <ToastContainer />
     </>
   );
 }
