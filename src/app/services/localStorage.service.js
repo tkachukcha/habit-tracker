9342154da7100fcc7ab2afc08c47d605c8cef474
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-02-20 18:55:55 +0300

    Add localStorage service

---
+++ src/app/services/localStorage.service.js
@@ -0,0 +1,51 @@
+const TOKEN_KEY = 'jwt-token';
+const REFRESH_KEY = 'jwt-refresh-token';
+const EXPIRES_KEY = 'jwt-expires';
+const USERID_KEY = 'user-local-id';
+
+export function setTokens({
+  refreshToken,
+  expiresIn = 3600,
+  idToken,
+  localId
+}) {
+  const expiresDate = new Date().getTime() + expiresIn * 1000;
+  localStorage.setItem(TOKEN_KEY, idToken);
+  localStorage.setItem(REFRESH_KEY, refreshToken);
+  localStorage.setItem(EXPIRES_KEY, expiresDate);
+  localStorage.setItem(USERID_KEY, localId);
+}
+
+export function getAccessToken() {
+  return localStorage.getItem(TOKEN_KEY);
+}
+
+export function getRefreshToken() {
+  return localStorage.getItem(REFRESH_KEY);
+}
+
+export function getExpiresToken() {
+  return localStorage.getItem(EXPIRES_KEY);
+}
+
+export function getUserIdToken() {
+  return localStorage.getItem(USERID_KEY);
+}
+
+export function removeAuthData() {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(REFRESH_KEY);
+  localStorage.removeItem(EXPIRES_KEY);
+  localStorage.removeItem(USERID_KEY);
+}
+
+const localStorageService = {
+  setTokens,
+  getAccessToken,
+  getExpiresToken,
+  getRefreshToken,
+  getUserIdToken,
+  removeAuthData
+};
+
+export default localStorageService;
