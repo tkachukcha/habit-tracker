06223932a364a865410b9bb0650a058a51597c9f
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-15 17:25:01 +0300

    Start add habit modal content

---
+++ src/app/components/ui/addHabitContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Divider } from '@mui/material';
+
+const AddHabitContent = () => {
+  return (
+    <>
+      <Typography variant="h5" gutterBottom>
+        Add Habit
+      </Typography>
+      <Divider />
+    </>
+  );
+};
+
+export default AddHabitContent;
--- src/app/components/ui/addHabitModal.jsx
+++ src/app/components/ui/addHabitModal.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
 import Backdrop from '@mui/material/Backdrop';
 import Fade from '@mui/material/Fade';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import AddHabitContent from './addHabitContent';
+
+const closeBtnStyle = {
+  position: 'absolute',
+  top: '0.3rem',
+  right: '0.3rem',
+  p: 1
+};
 
 const modalStyle = {
   position: 'absolute',
@@ -17,17 +24,9 @@ const modalStyle = {
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  elevation: 10,
-  p: 4,
+  p: 2,
   outline: 0,
   borderRadius: 3
-};
-
-const closeBtnStyle = {
-  position: 'absolute',
-  top: '0.3rem',
-  right: '0.3rem',
-  p: 1
 };
 
 const AddHabitModal = ({ onClose, open }) => {
@@ -48,6 +47,7 @@ const AddHabitModal = ({ onClose, open }) => {
           <IconButton sx={closeBtnStyle} onClick={onClose} color="primary">
             <CloseIcon sx={{ fontSize: '1.7rem' }} />
           </IconButton>
+          <AddHabitContent />
         </Box>
       </Fade>
     </Modal>
