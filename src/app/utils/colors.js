ad42915a82c0bb65384e7ee837b41a2a2a980733
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-28 17:41:56 +0300

    Change colors

--- src/app/utils/colors.js
+++ src/app/utils/colors.js
@@ -1,12 +1,12 @@
 const colors = [
-  'white',
-  'blue',
-  'yellow',
-  'purple',
-  'green',
-  'red',
-  'orange',
-  'gray'
+  '#ffffff', // white
+  '#3ea9ec', // blue
+  '#fd0d69', // red
+  '#db00cc', // purple
+  '#00f088', // green
+  '#6800ba', // dark purple
+  '#fcc800', // yellow
+  '#007b11' // dark green
 ];
 
 export default colors;
