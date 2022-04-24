538638693b33f9c521b9961f0cbf4c8dfd0f769f
Dan Tkachuk <d.tkachuk@yahoo.com>
2022-04-24 13:26:01 +0300

    Add day component

---
+++ client/src/app/components/ui/day.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+
+const Day = ({ date }) => {
+  return (
+    <>
+      <h1>Day</h1>
+    </>
+  );
+};
+Day.propTypes = {
+  date: PropTypes.object
+}
+
+export default Day;
--- client/src/app/components/ui/month.jsx
+++ client/src/app/components/ui/month.jsx
@@ -5,8 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getDays, getDaysData } from '../../store/days';
 
 const Month = ({ date }) => {
-  const firstDay = dayjs(date).date(1);
-  const monthDays = [];
   const dispatch = useDispatch();
   const days = useSelector(getDays());
 
@@ -19,11 +17,26 @@ const Month = ({ date }) => {
     }
   }, []);
 
-  // const month = firstDay.format('YYYY-MM-');
+  const monthLength = dayjs(date).daysInMonth();
+  const monthDays = [];
+
+  for (let i = 1; i <= monthLength; i++) {
+    monthDays.push(`${month}-${i < 10 ? `0${i}` : i}`);
+  }
+
+  // const firstDay = dayjs(date).date(1);
   // const weekDay = firstDay.format('d');
   // const weekDayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
-  return <></>;
+  return (
+    <>
+      <ul>
+        {monthDays.map((day) => (
+          <li key={day}>{day.slice(-2)}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 Month.defaultProps = {
   date: dayjs().format('YYYY-MM-DD')
